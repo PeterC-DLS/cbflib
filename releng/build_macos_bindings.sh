e6278eb8ca476eeef3da126bf53f6595cadf70b2
Peter Chang <peter.chang@diamond.ac.uk>
2022-04-22 10:58:16 +0100

    Specific minimum target

--- releng/build_macos_bindings.sh
+++ releng/build_macos_bindings.sh
@@ -7,6 +7,7 @@ ARCH=x86_64
 
 make Makefiles
 make distclean
+export MACOSX_DEPLOYMENT_TARGET=10.8
 make -f Makefile_OSX javawrapper javatests CBFLIB_DONT_BUILD_HDF5=yes CBF_NO_REGEX=yes CBFLIB_DONT_USE_LOCAL_REGEX=yes CBFLIB_DONT_USE_PYCIFRW=yes CBFLIB_DONT_USE_PY2CIFRW=yes CBFLIB_DONT_USE_PY3CIFRW=yes F90C='' CC="clang -arch $ARCH"
 
 JARFILE="jcbf/cbflib-*.jar"
@@ -21,6 +22,7 @@ X86_DEST=$DEST
 
 # Repeat for -arch arm64 (just build wrapper)
 ARCH=arm64
+export MACOSX_DEPLOYMENT_TARGET=11.0
 make -f Makefile_OSX javawrapper CBFLIB_DONT_BUILD_HDF5=yes CBF_NO_REGEX=yes CBFLIB_DONT_USE_LOCAL_REGEX=yes CBFLIB_DONT_USE_PYCIFRW=yes CBFLIB_DONT_USE_PY2CIFRW=yes CBFLIB_DONT_USE_PY3CIFRW=yes F90C='' CC="clang -arch $ARCH"
 
 DEST=dist/$VERSION/macos/$ARCH
